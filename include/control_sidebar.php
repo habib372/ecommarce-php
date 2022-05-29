8b8450bbd67f4baa24bd17cb77f235e9e758582a
Habibur Rahman (habib) <54948423+habib372@users.noreply.github.com>
2022-05-29 12:08:16 +0600

    Update control_sidebar.php

--- include/control_sidebar.php
+++ include/control_sidebar.php
@@ -1,5 +1,5 @@
 <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
+    <!-- Control sidebar content goes here-->
     <div class="p-3">
       <h5>Title</h5>
       <p>Sidebar content</p>
